6ed5c1103f537d59ca39eb69c1a98567a7051ce6
hanskin1908 <hanskch1993@hotmail.com>
2024-10-14 18:10:49 -0500

    Update vite.config.ts

--- sistemagestionescolar/vite.config.ts
+++ sistemagestionescolar/vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+   import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
-})
+   export default defineConfig({
+     plugins: [react()],
+     build: {
+       outDir: 'dist',
+     },
+   })
+   
