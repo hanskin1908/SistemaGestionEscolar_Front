b1b44a79114ef8b25665945f864b8902267e8830
hanskin1908 <hanskch1993@hotmail.com>
2024-10-16 00:10:21 -0500

    Update vite.config.ts

--- sistemagestionescolar/vite.config.ts
+++ sistemagestionescolar/vite.config.ts
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Asegúrate de que esto apunte al directorio que contiene index.html
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 })
